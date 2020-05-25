0fd32c74c783b27985668694050b93f48d123ca5
mattcrav <mattcrav@gmail.com>
2020-05-25 14:16:11 -0700

    Initial Commit

--- randomizerapp/views.py
+++ randomizerapp/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from randomizerapp.moo2randomizer import Result
+import os
 
 
 def index(request):
     result = Result()
     context = {'result': result}
-    return render(request, 'randomizerapp/index.html', context)
+    return HttpResponse(os.environ.get('SECRET_KEY'))
